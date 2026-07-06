5a6880e4b4eb3d8cb979a75ac5fc93fbceef4e1d
rameshgodara3841-web <rameshgodara3841@gmail.com>
2026-07-06 16:58:38 +0530

    Revise data generation to create 10,000 house entries
    Updated the data generation logic to create 10,000 entries with adjusted parameters for Bedrooms, Bathrooms, and House_Age. The output now includes the total number of rows generated.

--- generate_data.py
+++ generate_data.py
@@ -1,26 +1,4 @@
-# import pandas as pd
-# import random
-
-# data = []
-
-# for i in range(1, 1000):
-#    Area = random.randint(1, 10000)
-#    Bedrooms = random.randint(1, 12)
-#    Bathrooms = random.randint(1, 11)
-#    Hourse_Age= random.randint(1, 12)
-#    Location_Score = random.uniform(1, 10)
-
-# Price = (Area * 500) + (Bedrooms * 10000) + (Bathrooms * 1500) + (Hourse_Age * 10000) + (Location_Score * 5000)
-
-# data.append([Area, Bedrooms, Bathrooms, Hourse_Age, Location_Score, Price])
-
-# df = pd.DataFrame(data, columns=["Area", "Bedrooms", "Bathrooms", "Hourse_Age", "Location_Score", "Price"])
-# df.to_csv("house_data.csv", index=False)
-
-# print("Dataset generated successfully!")
-
-
-import pandas as pd
+ import pandas as pd
 import random
 
 data = []
