1b7aebfffe832115b71346570b0bc7c12b8c9b1c
rameshgodara3841-web <rameshgodara3841@gmail.com>
2026-07-06 17:40:08 +0530

    Refactor Flask app for better error handling

--- app.py
+++ app.py
@@ -1,30 +1,31 @@
- 
 from flask import Flask, render_template, request
 import joblib
 
- 
 app = Flask(__name__)
 
- 
+
 model = joblib.load("model.pkl")
+
 
 @app.route("/")
 def home():
-    return render_template("index.html", prediction_text=None)
+    return render_template("index.html", prediction_text="")
+
 
 @app.route("/predict", methods=["POST"])
 def predict():
-     
+    try:
+        
         area = float(request.form["Area"])
         bedrooms = int(request.form["Bedrooms"])
         bathrooms = int(request.form["Bathrooms"])
         house_age = int(request.form["House_Age"])
         location_score = float(request.form["Location_Score"])
 
- 
-        prediction = model.predict([
-            [area, bedrooms, bathrooms, house_age, location_score]
-        ])
+        
+        prediction = model.predict(
+            [[area, bedrooms, bathrooms, house_age, location_score]]
+        )
 
         predicted_price = prediction[0]
 
@@ -39,8 +40,13 @@ def predict():
             prediction_text=f"❌ Error: {str(e)}"
         )
 
-
-# Run Flask App
+ 
 if __name__ == "__main__":
-     app.run(debug=True) # apne laptop me ruk krne ke liye 
-  # app.run(host="0.0.0.0", port=5000, debug=True)  #dusare ip me run krne ke liye
+    app.run(debug=True)
+
+
+
+
+
+
+ 
